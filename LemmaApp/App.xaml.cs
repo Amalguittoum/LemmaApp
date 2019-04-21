617888eb27eeea6b8dac26b0d8eb994642c4571f
Amalguittoum <fa_guittoum@esi.dz>
2019-04-21 20:08:38 +0200

    add familly

---
+++ LemmaApp/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using LemmaApp.Views;
+using LemmaApp.Percistence;
+using System.IO;
+
+namespace LemmaApp
+{
+	public partial class App : Application
+	{
+        static BDD database;
+        public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+        public static BDD Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new BDD(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "BDDSQLite.db3"));
+                }
+                return database;
+            }
+        }
+
+        protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
